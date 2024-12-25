9b7825ee7b85a72c8c246d3d0c8295830fa02a5a
Giordano Lanna <167474669+isGLanna@users.noreply.github.com>
2024-12-25 01:29:00 -0300

    Update AbstractMenu.py

--- AbstractMenu.py
+++ AbstractMenu.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------
+# Program: Campo Minado
+# Developers: Giordano Lanna
+# Date: 24/12/2024
+# Language: Python 3.11
+# -----------------------------------------------
+
 import pygame
 from abc import ABC, abstractmethod
 
@@ -8,26 +15,9 @@ class Menu(ABC):
 
     @abstractmethod
     def draw_background(self):
-        self.background.draw_background()
-        self.background.transparent_surface()
 
-    def buttons(self): pass
+    @abstractmethod
+    def get_events(self): pass
 
-    def check_button_collision(self, event):
-        for button in self.buttons:
-            if button.collision_point(pygame.mouse.get_pos()):
-                button.up()
-                return button
-        return
-
-    def get_events(self):
-        for button in self.buttons:
-            if event.type == pygame.MOUSEBUTTONUP:
-                if button.collision_point(pygame.mouse.get_pos()):
-                    return button
-
-    def quitting(self):
-        pygame.quit()
-        sys.exit()
-
-    def __del__(self): pass
+    @abstractmethod
+    def quitting(self): pass
