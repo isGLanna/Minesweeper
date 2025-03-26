a0da6e79217c1d2b63e925828e4a783f23f56ca2
Giordano Lanna <167474669+isGLanna@users.noreply.github.com>
2025-03-26 11:43:31 -0300

    Fix
    Sintaxe error: were replaced " with ''' to conform to standard SQL formatting

--- Rank.py
+++ Rank.py
@@ -12,11 +12,15 @@ def create_table():
     database = sqlite3.connect("data/historic_player.db")
     cursor = database.cursor()
 
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='players'")
+    cursor.execute('''
+        SELECT name
+        FROM sqlite_master
+        WHERE type='table' AND name='players'
+        ''')
     if not cursor.fetchone():
         cursor.execute("CREATE TABLE  IF NOT EXISTS players (name text, time integer, level integer)")
         database.commit()
-    
+
     database.close()
 
 # insere 'players' no banco de dados
@@ -24,15 +28,26 @@ def insert_player(name, new_time, level):
     database = sqlite3.connect("data/historic_player.db")
     cursor = database.cursor()
 
-    cursor.execute("SELECT time FROM players WHERE name = ? AND level = ?", (name, level))
+    cursor.execute('''
+        SELECT time
+        FROM players
+        WHERE name = ? AND level = ?''', (name, level))
     result = cursor.fetchone()
 
     if result:
         time = result[0]
         if new_time < time:
-            cursor.execute("UPDATE players SET time = ? WHERE name = ? AND level = ?", (name, new_time, level))
+            cursor.execute('''
+            UPDATE players
+            SET time = ?
+            WHERE name = ? AND level = ?''',
+            (name, new_time, level))
     else:
-        cursor.execute("INSERT INTO players (name, time, level) VALUES (?, ?, ?)", (name, new_time, level))
+        cursor.execute('''
+        INSERT INTO players
+        (name, time, level)
+        VALUES (?, ?, ?)''',
+        (name, new_time, level))
 
     print("tentando salvar")
     database.commit()
@@ -43,7 +58,10 @@ def clean_history():
     database = sqlite3.connect("data/historic_player.db")
     cursor = database.cursor()
 
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    cursor.execute('''
+    SELECT name
+    FROM sqlite_master
+    WHERE type='table''')
     all_tables = cursor.fetchall()
 
     for table in all_tables:
@@ -58,10 +76,20 @@ def fetch_players():
     database = sqlite3.connect("data/historic_player.db")
     cursor = database.cursor()
 
-    cursor.execute('SELECT name, time FROM players WHERE level = 1 ORDER BY time ASC')
+    cursor.execute('''
+        SELECT name, time
+        FROM players
+        WHERE level = 1
+        ORDER BY time ASC
+        ''')
     easy_level_row = cursor.fetchall()
 
-    cursor.execute('SELECT name, time FROM players WHERE level = 0 ORDER BY time ASC')
+    cursor.execute('''
+        SELECT name, time
+        FROM players
+        WHERE level = 0
+        ORDER BY time ASC
+        ''')
     hard_level_row = cursor.fetchall()
 
     database.close()
@@ -69,5 +97,3 @@ def fetch_players():
     return easy_level_row, hard_level_row
 
 create_table()
-
-
